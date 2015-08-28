967c437f647e467ab0eaec7fd6ead58805f97355
Mateo bossio <mateofede@hotmail.com>
2015-08-28 10:27:52 -0300

    agregados las clases al admin

--- PikQuick/blog/admin.py
+++ PikQuick/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import User, Post, Coment, Options
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Coment)
+admin.site.register(Coment)
