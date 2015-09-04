9c9de164b6ba57cf01673beab45c8183978e210f
Mateo bossio <mateofede@hotmail.com>
2015-09-04 08:20:50 -0300

    Modelos hechos, funcionan

--- PikQuick/blog/models.py
+++ PikQuick/blog/models.py
@@ -2,31 +2,26 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField('id', max_lenth 15)
-    mail = models.CharField('mail', max_lenth 30)
-    password = models.CharField('password', max_lenth 10)
+    name = models.CharField('id', max_length=15, unique=True)
+    mail = models.CharField('mail', max_length=30)
+    password = models.CharField('password', max_length=10)
     def __str__(self):
         return self.id
 
 class Post(models.Model):
     class Meta:
-        ordering=['-fecha_pub']
-    desscription = models.CharField('description', max_lenth 150)
-    date = models.DateTimeField('fecha', auto_now_true_True)
+        ordering=['-date']
+    desscription = models.CharField('description', max_length=150)
+    date = models.DateTimeField('fecha', auto_now_add=True)
     user = models.ForeignKey(User)
     def __str__(self):
         return self.description
 
 class Coment(models.Model):
     class Meta:
-        ordering=['-fecha_pub']
-    content = models.TextFiled('content', max_lenth 150)
+        ordering=['-date']
+    date = models.DateTimeField('fecha', auto_now_add=True)
+    content = models.TextField('content', max_length=150)
     user = models.ForeignKey(User)
     def __str__(self):
         return self.content
-
-class Options(models.Model):
-    img = models.ImageField('photo')
-    description = models.TextField('content', max_lenth 15)
-    def __str__(self):
-        return self.photo
